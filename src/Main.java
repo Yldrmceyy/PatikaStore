a7dfd83e690c631ae7ce68bb5a7d302b4a937b33
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-03-24 14:12:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        PatikaStore patikaStore = new PatikaStore();
+        patikaStore.showMenu();
+    }
+
+}
